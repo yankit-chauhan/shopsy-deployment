1fd62efa5c3eab877f882772ff2c51d057e3fc0d
YANKIT CHAUHAN <yankit.chauhan@nagarro.com>
2026-04-15 12:23:15 +0530

    changes

--- apply-all.sh
+++ apply-all.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
-kubectl apply -f k8s/namespace.yaml
-kubectl apply -f k8s/secrets.yaml
-kubectl apply -f k8s/postgres/
-kubectl apply -f k8s/product-service/
-kubectl apply -f k8s/search-service/
-kubectl apply -f k8s/auth-service/
-kubectl apply -f k8s/api-gateway/
-kubectl apply -f k8s/ingress.yaml
+kubectl apply -f namespace.yaml
+kubectl apply -f secrets.yaml
+kubectl apply -f postgres/
+kubectl apply -f product-service/
+kubectl apply -f search-service/
+kubectl apply -f auth-service/
+kubectl apply -f api-gateway/
+kubectl apply -f ingress.yaml
 kubectl get all -n shopsy
 kubectl get ingress -n shopsy
--- delete-all.sh
+++ delete-all.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
-kubectl delete -f k8s/ingress.yaml --ignore-not-found=true
-kubectl delete -f k8s/api-gateway/ --ignore-not-found=true
-kubectl delete -f k8s/auth-service/ --ignore-not-found=true
-kubectl delete -f k8s/search-service/ --ignore-not-found=true
-kubectl delete -f k8s/product-service/ --ignore-not-found=true
-kubectl delete -f k8s/postgres/ --ignore-not-found=true
-kubectl delete -f k8s/secrets.yaml --ignore-not-found=true
-kubectl delete -f k8s/namespace.yaml --ignore-not-found=true
+kubectl delete -f ingress.yaml --ignore-not-found=true
+kubectl delete -f api-gateway/ --ignore-not-found=true
+kubectl delete -f auth-service/ --ignore-not-found=true
+kubectl delete -f search-service/ --ignore-not-found=true
+kubectl delete -f product-service/ --ignore-not-found=true
+kubectl delete -f postgres/ --ignore-not-found=true
+kubectl delete -f secrets.yaml --ignore-not-found=true
+kubectl delete -f namespace.yaml --ignore-not-found=true
